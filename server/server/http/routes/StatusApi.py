01db0aa3f3b91f7d6c0e965cd89a2a132a2be138
Albert Knudsen <kcobain465@gmail.com>
2025-04-28 03:06:00 +0200

    This commit implements the retrieval of the server region using ipinfo.io and generates the `SERVER_FEATURES_` dictionary based on the region data.

--- server/server/http/routes/StatusApi.py
+++ server/server/http/routes/StatusApi.py
@@ -8,22 +8,15 @@ from utils.Errors import Codes  # Import the Router instance (see step 3)
 from dataclasses import fields
 import requests
 
-def get_server_region():
-    try:
-        response = requests.get("https://ipinfo.io/json")
-        data = response.json()
-        res = {
-            "SHORT": f"{data.get('timezone').split('/')[0].capitalize()}-{data.get('country')}",
-            "COUNTRY": f"{data.get('country')}-{data.get('region').lowercase().replace(' ', '_')}"
-        }
-        return res
-    except Exception as e:
-        print(f"Error getting region: {e}")
-        return None
 
 
 async def status_handler(request: web.Request, services: AppServices):
-    region = get_server_region()
+    response = requests.get("https://ipinfo.io/json")
+    data = response.json()
+    region = {
+        "SHORT": f"{data.get('timezone').split('/')[0].capitalize()}-{data.get('country')}",
+        "COUNTRY": f"{data.get('country')}-{data.get('region').lowercase().replace(' ', '_')}"
+    }
     
     prefix = "SERVER_FEATURES_"
     server_features = {
