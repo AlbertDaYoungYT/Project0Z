8ae3e0910b476c0083156721efb9c5d3b5bc10d6
Albert Knudsen <kcobain465@gmail.com>
2025-05-01 01:34:51 +0200

    Fixed Public key not wanting to Export

--- server/server/http/routes/auth/InitAck.py
+++ server/server/http/routes/auth/InitAck.py
@@ -5,16 +5,18 @@ from aiohttp import web
 from utils.AppServices import AppServices
 from utils.Errors import Codes  # Import the Router instance (see step 3)
 
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
+
 async def initial_ack_handler(request: web.Request, services: AppServices):
     _json: dict = json.loads(request.content.read_nowait())
 
     loguru.logger.debug(f"Initial ACK Response from {request.remote}")
     id = _json.get("id", None)
     if id == None: return web.json_response(Codes.ITEM_NOT_FOUND.value.to_dict())
-    loguru.logger.debug(services.ca_authority.root_private_key.public_key().public_bytes("Raw", "Raw"))
+    loguru.logger.debug(services.ca_authority.root_private_key.public_key().public_bytes(Encoding.Raw, PublicFormat.Raw))
     res = {
         "id": id,
-        "public_key": base64.urlsafe_b64encode(services.ca_authority.root_private_key.public_key().public_bytes("Raw", "Raw")).decode()
+        "public_key": base64.urlsafe_b64encode(services.ca_authority.root_private_key.public_key().public_bytes(Encoding.Raw, PublicFormat.Raw)).decode()
     }
 
     return web.json_response(res)
